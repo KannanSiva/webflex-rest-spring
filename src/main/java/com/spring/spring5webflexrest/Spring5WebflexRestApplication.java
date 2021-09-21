947fa591dfbf259bf72d8dcc34a1e8632a2eb3b2
Makankan <matadorsmca@gmail.com>
2021-09-21 13:03:25 +0530

    first commit

---
+++ src/main/java/com/spring/spring5webflexrest/Spring5WebflexRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring5webflexrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebflexRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebflexRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spring5webflexrest/Spring5WebflexRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring5webflexrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebflexRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
